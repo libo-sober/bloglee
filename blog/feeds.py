45782f936e086bfc26b1e872ebc1ab36ef02b9c5
LiBo <libo_sober@163.com>
2021-04-02 21:54:33 +0800

    参数跟随

--- blog/feeds.py
+++ blog/feeds.py
@@ -12,7 +12,9 @@ class BlogRssFeed(Feed):
         return Article.objects.all()
     def item_title(self, item):
         return item.title
+    def get_image_url(self, url):
+        return self.request.build_absolute_uri('/image{}'.format(url))
     def item_description(self, item):
         return item.content
     def item_link(self, item):
-        return reverse('detail', args=[item.id,])
+        return reverse('detail', args=[item.en_us,])
